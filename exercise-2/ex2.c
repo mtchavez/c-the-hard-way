35f986594de6da0aea42e16e930b51d7927d9245
Chavez <nailsbrokeandfell@gmail.com>
2013-04-04 16:59:07 -0700

    Exercise 3

---
+++ exercise-2/ex2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  puts("Hello World");
+  printf("%f\n", 30.0);
+  for (int i = 0; i < 5; ++i)
+  {
+    printf("%d\n", i);
+  }
+  return 0;
+}
