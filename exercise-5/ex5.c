a938b5703ccb2807a97cd24539c05e926b954baa
Chavez <nailsbrokeandfell@gmail.com>
2013-04-04 17:19:30 -0700

    Exercise 5

---
+++ exercise-5/ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* A Comment */
+int main(int argc, char const *argv[])
+{
+  int distance = 100;
+
+  // Comment
+  printf("You are %d miles away.\n", distance);
+
+  return 0;
+}
