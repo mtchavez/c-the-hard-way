00df13d80d06059bd2dc9264ab1a89d8340ce08b
Chavez <nailsbrokeandfell@gmail.com>
2013-04-06 15:10:14 -0700

    Exercise 8 extra credit.

--- exercise-8/ex8.c
+++ exercise-8/ex8.c
@@ -3,13 +3,16 @@
 
 int main(int argc, char const *argv[])
 {
-  int areas[] = {10, 12, 13, 14, 20};
+  int areas[] = {10, 12, 13, 14, 20, 100};
   char name[] = "Chavez";
   char full_name[] = {
     'M', ' ',
     'C', 'h', 'a', 'v', 'e', 'z', '\0'
   };
-
+  areas[0] = 100;
+  areas[1] = 'X';
+  name[5] = 'Z';
+  full_name[1] = '.';
   printf("The size of an int %ld\n", sizeof(int));
   printf("The size of areas (int[]) %ld\n", sizeof(areas));
   printf("The number of ints in areas are %ld\n", sizeof(areas)/sizeof(int));
