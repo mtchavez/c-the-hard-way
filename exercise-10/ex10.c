fd6aac119a0378feda0e66aec0613fe2acf00ade
Chavez <nailsbrokeandfell@gmail.com>
2013-04-06 22:59:50 -0700

    Exercise 10 extra credit

--- exercise-10/ex10.c
+++ exercise-10/ex10.c
@@ -14,8 +14,8 @@ int main(int argc, char const *argv[])
     "Texas"
   };
 
-  for (int i = 0; i < 4; ++i) {
-    printf("State %d is %s\n", i, states[i]);
+  for (int i = 0, j = 4; i < 4; ++i, j--) {
+    printf("State %d is %s J is %d\n", i, states[i], j);
   }
   return 0;
 }
