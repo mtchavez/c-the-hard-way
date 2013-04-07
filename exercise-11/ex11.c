2075a42cc2edee852a8b5d43e2eb6a3d16440fbb
Chavez <nailsbrokeandfell@gmail.com>
2013-04-07 08:20:29 -0700

    Exercise 11 extra credit.

--- exercise-11/ex11.c
+++ exercise-11/ex11.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
+#define SIZE(x) (sizeof(x) / sizeof(x[0]))
 
 int main(int argc, char const *argv[])
 {
-  int i = 0;
-  while(i < argc){
-    printf("arg %d is %s\n", argc, argv[i]);
-    i++;
-  }
-
   char *states[] = {
     "California",
     "Oregon",
@@ -16,11 +11,20 @@ int main(int argc, char const *argv[])
     "Texas"
   };
 
-  i = 0;
-  int num_states = 4;
-  while(i < num_states){
-    printf("State %d is %s\n", i, states[i]);
+  int i = 0;
+  while(i < argc){
+    printf("arg %d is %s\n", argc, argv[i]);
+    if (i < SIZE(states)){
+      states[i] = argv[i];
+    }
     i++;
+  }
+
+  int num_states = SIZE(states);
+  i = num_states - 1;
+  while(i < num_states && i >= 0){
+    printf("State %d is %s\n", i, states[i]);
+    i--;
   }
 
   return 0;
