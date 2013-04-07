f0e0ef79abaaf419b30918bce9075bb0bd0d3b79
Chavez <nailsbrokeandfell@gmail.com>
2013-04-07 09:44:23 -0700

    Exercise 12 extra credit.

--- exercise-12/ex12.c
+++ exercise-12/ex12.c
@@ -6,8 +6,8 @@ int main(int argc, char const *argv[])
   if (argc == 1){
     printf("Not enough arguments.\n");
   }
-  else if (argc > 1 && argc < 4){
-    for (int i = 0; i < argc; i++){
+  else if (argc >= 2 && argc < 4){
+    for (int i = 1; i < argc; i++){
       printf("Arg[%d] = %s\n", i, argv[i]);
     }
   }
